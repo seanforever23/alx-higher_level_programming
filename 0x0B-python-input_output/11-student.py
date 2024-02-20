7709eb33ba1a8226085fd36f68e66bbf4ffa27dc
seanforever23 <seanwanton18@gmail.com>
2024-02-20 13:33:27 -0800

    11

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,41 +1,33 @@
 #!/usr/bin/python3
-"""Creates a class Student."""
+
+"""Student class to represent a student"""
 
 
 class Student:
-    """Initialzes a student."""
-
+    """Defining student class """
     def __init__(self, first_name, last_name, age):
-        """Initialize a new Student.
-
+        """Student constructor
         Args:
-            first_name (str): The first name of the student.
-            last_name (str): The last name of the student.
-            age (int): The age of the student.
+            first_name(str)
+            last_name(str)
+            age(str)
         """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Return a dictionary representation of the Student.
-
-        If attrs is a list of strings, represents only those attributes
-        included in the list.
-
+        """ Retrive information about an instance to json
         Args:
-            attrs (list): (Optional) The attributes to represent.
+            attrs(list) -> List of attribute to be retrieved
         """
-        def get_attributes(self, attrs):
-            if isinstance(attrs, list) and all(isinstance(ele, str) for ele in attrs):
-                return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
-            return self.__dict__
+        if (type(attrs) == list and
+                all(type(element) == str for element in attrs)):
+            return {key: getattr(self, key)
+                    for key in attrs if hasattr(self, key)}
+        return self.__dict__
 
     def reload_from_json(self, json):
-        """Change all attributes of the Student.
-
-        Args:
-            json (dict): The key/value pairs to replace attributes with.
-        """
-        for k, v in json.items():
-            setattr(self, k, v)
+        """Replace attributes of the object with k-v pair in the json """
+        for key, value in json.items():
+            setattr(self, key, value)
