f18f20f3b70688e592c18a7050b53704034ba3de
seanforever23 <seanwanton18@gmail.com>
2024-04-28 11:41:27 -0700

    0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python3
-""" This python script lists all states from the database hbtn_0e_0_usa
-"""
+"lists all states from the database hbtn_0e_0_usa"
 import MySQLdb
-from sys import argv
+import sys
 
-if __name__ == " __main__":
-    # creating a connection to the database with args from the cli
-    d_b = MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2], db=argv[3], port=3306)
-    # creating a cursor object
-    cursor = d_b.cursor()
-    # executing the query using the corspr object an execute function
-    cursor.execute("SELECT * FROM states ORDER BY id")
-    # fetching and printing query result
-    rows = cursor.fetchall()
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost", port=3306, user=sys.argv[1],
+        password=sys.argv[2], database=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+    rows = cur.fetchall()
     for row in rows:
         print(row)
-    # close all open cursors
-    cursor.close()
-    # close the database
-    d_b.close()
+    cur.close()
+    db.close()
